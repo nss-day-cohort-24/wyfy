3e358f0fa76789bfed17630cbe989644733ff53e
Jesie Oldenburg <kitglo46@gmail.com>
2018-05-16 18:51:47 -0500

    Initial setup for google maps; map is now rendering

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navigation from './Nav';
+import MapContainer from './Map';
+
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Navigation/>
+        <MapContainer />
       </div>
     );
   }
