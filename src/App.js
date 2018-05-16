794c4c8832cccb0bdca5960511d308f420b9cc6f
Meg Scholl <meghan.e.scholl@gmail.com>
2018-05-16 16:17:40 -0500

    scss file with fonts and colors, logo setup, and styleguidist

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import LargeLogo from './components/Logo';
+import './main.scss';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <LargeLogo />
+        <h1>sup</h1>
       </div>
-    );
+    )
   }
 }
 
