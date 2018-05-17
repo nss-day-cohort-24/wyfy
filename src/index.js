38a20a71e59da2357e245ad27cefd656c3ee2f7d
Taylor Bailey <taylor.williams.bailey@gmail.com>
2018-05-17 10:17:12 -0500

    moved map.js and nav.js into component folder

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './main.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
