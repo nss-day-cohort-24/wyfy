2b0481fe004765f283356c1097a876fc4b608250
Ben Atkins <benjatkins13@gmail.com>
2018-05-17 10:14:47 -0500

    commit to merge

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import NashData from './components/NashData';
 
 class App extends Component {
   render() {
     return (
       <div>
+      <NashData />
       </div>
     );
   }
