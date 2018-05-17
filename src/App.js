b66afa62046c626d255f9f764832a2c7a0362ace
Meg Scholl <meghan.e.scholl@gmail.com>
2018-05-17 09:41:26 -0500

    resolved second conflict

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-<<<<<<< HEAD
 import Navigation from './Nav';
 import MapContainer from './Map';
-
-=======
 import LargeLogo from './components/LargeLogo';
-import './main.scss';
->>>>>>> origin
+
 
 class App extends Component {
   render() {
     return (
       <div>
-<<<<<<< HEAD
+
         <Navigation/>
         <MapContainer />
-=======
         <LargeLogo />
-        <h1>sup</h1>
->>>>>>> origin
+
       </div>
     )
   }
