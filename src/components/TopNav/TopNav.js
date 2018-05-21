5505793825d4e325e199a2589c95fe8585bcca68
Taylor Bailey <taylor.williams.bailey@gmail.com>
2018-05-21 11:14:51 -0500

    added search bar to top nav component and styles

---
+++ src/components/TopNav/TopNav.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import Logo4 from '../Logos/Logo4';
+import './topnav.css';
+
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  Input,
+  DropdownItem } from 'reactstrap';
+
+export default class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar className="d-flex justify-content-between" color="light" light expand="md">         
+          <Logo4 />
+          <Input className="col-5 searchBar" type="search" name="search" id="search" placeholder="Enter Zip..." />
+        </Navbar>
+      </div>
+    );
+  }
+}
