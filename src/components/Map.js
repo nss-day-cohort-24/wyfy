b7893dafe864741ef829d9f3f19462cd27d5ddef
Sam Kimball <s.j.kimball@icloud.com>
2018-05-22 16:23:20 -0500

    Reverse lat and long and map all points

--- src/components/Map.js
+++ src/components/Map.js
@@ -7,25 +7,24 @@ console.log('map here');
 var API_KEY = 'AIzaSyBFArv5hJebxtu-iUHl_XQYlq4gmuf2Xeo';
 
 export class MapContainer extends Component {
+
   render() {
     return (
     <div className="map-flexbox">
-      <Map google={this.props.google} data={this.props.data} zoom={14} className="main-map">
-        <Marker name={'Current location'} />
+      <Map google={this.props.google} zoom={9} className="main-map">
 
-        this.props.data.map(index) {
-          <Marker
-            title={this.props.data.site_name}
-            name={this.props.data.site_name}
-            position={this.props.latlon}
-          />
+        <Marker title="current location" />
+
+        {
+          this.props.data.map((item, index) => (
+            <Marker
+              title={item.site_name}
+              name={item.site_name}
+              position={{lat: item.mapped_location.coordinates[1], lng: item.mapped_location.coordinates[0]}}
+            />
+          ))
         }
 
-        <Marker
-          title={'Adams Kart Track'}
-          name={'Adams Kart Track'}
-          position={{lat: 34.0082, lng: -117.3828}}
-        />
       </Map>
     </div>
     );
